b40fa8e372fcf9edc6cf697399d65b624fdd053d
unknown <bosu15-5645@diu.edu.bd>
2019-04-28 03:37:03 +0600

    array using c#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[10]; //for string type
+            names[0] = "shuvo";
+            names[1] = "sabuj";
+            names[2] = "dipok";
+            names[3] = "dipto";
+            string aName = names[2];
+            //Console.WriteLine(aName);
+
+            int[] numberlist = new int[3]; // for integer type
+            numberlist[0] = 10;
+            numberlist[1] = 20;
+            int aNumberlist = numberlist[0];
+            Console.WriteLine(aNumberlist);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
